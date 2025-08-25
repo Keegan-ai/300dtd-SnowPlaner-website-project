bef86eab623db9e47c81130525597083984ce1c1
Keegan-ai <kgous@waimea.school.nz>
2025-08-26 09:59:30 +1200

    Finally created a group creator
    Im thinking of having it so that people can create their own password because it would be easier for them to share it and for me to connect them to a group

--- app/__init__.py
+++ app/__init__.py
@@ -43,6 +43,15 @@ def main():
     return render_template("pages/main.jinja")
 
 
+
+#-----------------------------------------------------------
+# Join group page route
+#-----------------------------------------------------------
+@app.get("/join")
+def join_group():
+    return render_template("pages/join_group.jinja")
+
+
 #-----------------------------------------------------------
 # About page route
 #-----------------------------------------------------------
@@ -53,45 +62,31 @@ def group():
 #-----------------------------------------------------------
 # About page route
 #-----------------------------------------------------------
-@app.get("/create_group/")
-def create_group():
-    return render_template("pages/create_group.jinja")
+@app.get("/creator")
+def group_creator():
+    return render_template("pages/group_creator.jinja")
 
 
 #-----------------------------------------------------------
-# Join group page route
+# Create group rout
 #-----------------------------------------------------------
-@app.get("/join")
-def join_group():
-    return render_template("pages/join_group.jinja")
-
-
-
-#-----------------------------------------------------------
-# Create group page route
-#-----------------------------------------------------------
-@app.get("/create-group")
+@app.post("/create-group")
 @login_required
 def create_group(): 
-    
-# Get the data from the form
+    # Get the data from the form
     group_name = request.form.get("group_name")
-   
 
     with connect_db() as client:
         # Attempt to find an existing record for that user
-        print (group_name)
-        sql = "SELECT * FROM group WHERE group_name = ?"
+        sql = "SELECT * FROM `group` WHERE group_name = ?"
         params = [group_name]
         result = client.execute(sql, params)
 
         # No existing record found, so safe to add the user
         if not result.rows:
-            # Sanitise the name
-            group_name = html.escape(group_name)
 
             # Add the user to the users table
-            sql = "INSERT INTO group (group_name) VALUES (?)"
+            sql = "INSERT INTO `group` (group_name) VALUES (?)"
             params = [group_name]
             client.execute(sql, params)
 
@@ -100,115 +95,116 @@ def create_group():
             return redirect("/main")
 
         # Found an existing record, so prompt to try again
-        flash("Group failed to create", "error")
-        return redirect("/create_group")
+        flash("Group already exist pls try again", "error")
+        return redirect("/creator")
 
 
-#-----------------------------------------------------------
-# Things page route - Show all the things, and new thing form
-#-----------------------------------------------------------
-@app.get("/things/")
-def show_all_things():
-    with connect_db() as client:
-        # Get all the things from the DB
-        sql = """
-            SELECT things.id,
-                   things.name,
-                   users.name AS owner
 
-            FROM things
-            JOIN users ON things.user_id = users.id
+# #-----------------------------------------------------------
+# # Things page route - Show all the things, and new thing form
+# #-----------------------------------------------------------
+# @app.get("/things/")
+# def show_all_things():
+#     with connect_db() as client:
+#         # Get all the things from the DB
+#         sql = """
+#             SELECT things.id,
+#                    things.name,
+#                    users.name AS owner
 
-            ORDER BY things.name ASC
-        """
-        params=[]
-        result = client.execute(sql, params)
-        things = result.rows
+#             FROM things
+#             JOIN users ON things.user_id = users.id
 
-        # And show them on the page
-        return render_template("pages/things.jinja", things=things)
+#             ORDER BY things.name ASC
+#         """
+#         params=[]
+#         result = client.execute(sql, params)
+#         things = result.rows
+
+#         # And show them on the page
+#         return render_template("pages/things.jinja", things=things)
 
 
-#-----------------------------------------------------------
-# Thing page route - Show details of a single thing
-#-----------------------------------------------------------
-@app.get("/thing/<int:id>")
-def show_one_thing(id):
-    with connect_db() as client:
-        # Get the thing details from the DB, including the owner info
-        sql = """
-            SELECT things.id,
-                   things.name,
-                   things.price,
-                   things.user_id,
-                   users.name AS owner
+# #-----------------------------------------------------------
+# # Thing page route - Show details of a single thing
+# #-----------------------------------------------------------
+# @app.get("/thing/<int:id>")
+# def show_one_thing(id):
+#     with connect_db() as client:
+#         # Get the thing details from the DB, including the owner info
+#         sql = """
+#             SELECT things.id,
+#                    things.name,
+#                    things.price,
+#                    things.user_id,
+#                    users.name AS owner
 
-            FROM things
-            JOIN users ON things.user_id = users.id
+#             FROM things
+#             JOIN users ON things.user_id = users.id
 
-            WHERE things.id=?
-        """
-        params = [id]
-        result = client.execute(sql, params)
+#             WHERE things.id=?
+#         """
+#         params = [id]
+#         result = client.execute(sql, params)
 
-        # Did we get a result?
-        if result.rows:
-            # yes, so show it on the page
-            thing = result.rows[0]
-            return render_template("pages/thing.jinja", thing=thing)
+#         # Did we get a result?
+#         if result.rows:
+#             # yes, so show it on the page
+#             thing = result.rows[0]
+#             return render_template("pages/thing.jinja", thing=thing)
 
-        else:
-            # No, so show error
-            return not_found_error()
+#         else:
+#             # No, so show error
+#             return not_found_error()
 
 
 #-----------------------------------------------------------
 # Route for adding a thing, using data posted from a form
 # - Restricted to logged in users
-#-----------------------------------------------------------
-@app.post("/add")
-@login_required
-def add_a_thing():
-    # Get the data from the form
-    name  = request.form.get("name")
-    price = request.form.get("price")
+# #-----------------------------------------------------------
+# @app.post("/add")
+# @login_required
+# def add_a_thing():
+#     # Get the data from the form
+#     name  = request.form.get("name")
+#     price = request.form.get("price")
 
-    # Sanitise the text inputs
-    name = html.escape(name)
+#     # Sanitise the text inputs
+#     name = html.escape(name)
 
-    # Get the user id from the session
-    user_id = session["user_id"]
+#     # Get the user id from the session
+#     user_id = session["user_id"]
 
-    with connect_db() as client:
-        # Add the thing to the DB
-        sql = "INSERT INTO things (name, price, user_id) VALUES (?, ?, ?)"
-        params = [name, price, user_id]
-        client.execute(sql, params)
+#     with connect_db() as client:
+#         # Add the thing to the DB
+#         sql = "INSERT INTO things (name, price, user_id) VALUES (?, ?, ?)"
+#         params = [name, price, user_id]
+#         client.execute(sql, params)
 
-        # Go back to the home page
-        flash(f"Thing '{name}' added", "success")
-        return redirect("/things")
+#         # Go back to the home page
+#         flash(f"Thing '{name}' added", "success")
+#         return redirect("/things")
 
 
-#-----------------------------------------------------------
-# Route for deleting a thing, Id given in the route
-# - Restricted to logged in users
-#-----------------------------------------------------------
-@app.get("/delete/<int:id>")
-@login_required
-def delete_a_thing(id):
-    # Get the user id from the session
-    user_id = session["user_id"]
+# #-----------------------------------------------------------
+# # Route for deleting a thing, Id given in the route
+# # - Restricted to logged in users
+# #-----------------------------------------------------------
+# @app.get("/delete/<int:id>")
+# @login_required
+# def delete_a_thing(id):
+#     # Get the user id from the session
+#     user_id = session["user_id"]
 
-    with connect_db() as client:
-        # Delete the thing from the DB only if we own it
-        sql = "DELETE FROM things WHERE id=? AND user_id=?"
-        params = [id, user_id]
-        client.execute(sql, params)
+#     with connect_db() as client:
+#         # Delete the thing from the DB only if we own it
+#         sql = "DELETE FROM things WHERE id=? AND user_id=?"
+#         params = [id, user_id]
+#         client.execute(sql, params)
 
-        # Go back to the home page
-        flash("Thing deleted", "success")
-        return redirect("/things")
+#         # Go back to the home page
+#         flash("Thing deleted", "success")
+#         return redirect("/things")
 
 
 
